094f1f70af0a311f12e9c1f75e6517128fc037f2
Kunal Dabir <kunal.dabir@gmail.com>
2022-05-06 10:49:26 +0530

    upgrade manifest to 3

--- background.js
+++ background.js
@@ -1,9 +1,17 @@
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'mail.google.com' },
-        })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+// Wrap in an onInstalled callback in order to avoid unnecessary work
+// every time the background script is run
+chrome.runtime.onInstalled.addListener(() => {
+  // Page actions are disabled by default and enabled on select tabs
+  chrome.action.disable();
+
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostEquals: 'mail.google.com' },
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+
 });
